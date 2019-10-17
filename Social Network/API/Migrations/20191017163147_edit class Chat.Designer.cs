0241fa8e89e0a03568808ee9ad58e7c382403599
Tereschenko <Tereschenko@A25PC2>
2019-10-17 19:42:55 +0300

    Edit class Chat

--- Social Network/API/Migrations/20191017163147_edit class Chat.Designer.cs
+++ Social Network/API/Migrations/20191017163147_edit class Chat.Designer.cs
@@ -3,14 +3,16 @@ using API.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Migrations
 {
     [DbContext(typeof(EFContext))]
-    partial class EFContextModelSnapshot : ModelSnapshot
+    [Migration("20191017163147_edit class Chat")]
+    partial class editclassChat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
